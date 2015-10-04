9122d2e78afd18f1592d68238b7f62c8b8d42bb8
talebilling <talebilling@gmail.com>
2015-10-04 23:13:28 +0200

    checkstyle

--- hello-world/hello-world/middleearth_consol/MiddleEarth.java
+++ hello-world/hello-world/middleearth_consol/MiddleEarth.java
@@ -3,19 +3,19 @@ package middleearth_consol;
 public class MiddleEarth {
 
 	public static void main(String[] args) {
-	Elf elf1 = new Elf("Legolas");
-	Elf elf2 = new Elf("Arwen");
-	Elf elf3 = new Elf("Elrond");
-	Elf elf4 = new Elf("Galadriel");
-			
-	elf1.sayNumber(); 
-	elf2.sayNumber();
-	elf3.sayNumber();
-	elf4.sayNumber();
-	
-	Elf.printTotalNumberOfElves();
-	
-	System.out.println(elf1.getName());
+		Elf elf1 = new Elf("Legolas");
+		Elf elf2 = new Elf("Arwen");
+		Elf elf3 = new Elf("Elrond");
+		Elf elf4 = new Elf("Galadriel");
+
+		elf1.sayNumber(); 
+		elf2.sayNumber();
+		elf3.sayNumber();
+		elf4.sayNumber();
+
+		Elf.printTotalNumberOfElves();
+
+		System.out.println(elf1.getName());
 	}
 
 }
