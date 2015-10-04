7dac678ac469583ef08e65fad0baa0d738055fdd
talebilling <talebilling@gmail.com>
2015-10-04 19:48:02 +0200

    initial commit

---
+++ paprika/paprika/src/paprika/Paprika.java
@@ -0,0 +1,9 @@
+package paprika;
+
+public class Paprika {
+	public static void main(String[] args) {
+		double alma=5.3,korte=2.3,barack=4;
+		double ossz=alma+korte+barack;
+		System.out.println(ossz);
+	}		
+}
---
+++ paprika/paprika/src/paprika/Paprika2.java
@@ -0,0 +1,10 @@
+package paprika;
+
+public class Paprika2 {
+	public static void main(String[] args) {
+		//char hamis='a' -egy karaktert fog kiírni
+		
+		boolean hamis = false;
+		System.out.println(hamis);
+	}		
+}
