1b8f241a2fcafb5eca90ed573bf6553613574f43
talebilling <talebilling@gmail.com>
2015-10-08 16:16:50 +0200

    First looping with operations.

--- hello-world/hello-world/spagetti/Spagetti.java
+++ hello-world/hello-world/spagetti/Spagetti.java
@@ -13,8 +13,26 @@ public class Spagetti {
 		if("sum".equals(operation)) {
 			System.out.println(number1+number2);
 		}
-//		else if("Feltétel"){
-//			System.out.println("I dont know this operation!");
-//		}
+
+		else if ("diff".equals(operation)){
+			System.out.println(number1-number2);
+		}
+
+		else if ("multi".equals(operation)){
+			System.out.println(number1*number2);
+		}
+
+		else if ("ratio".equals(operation)){
+			System.out.println(number1/number2);
+		}
+
+		//		else if("Feltétel"){
+		//			System.out.println("I dont know this operation!");
+		//		}
+
+		else {
+			System.out.println("Something went wrong, I cannot do the operation!");
+			System.out.println("Please type an operation: sum / diff / multi / ratio, and 2 numbers.");
+		}
 	}
 }
