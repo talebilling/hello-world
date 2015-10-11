b02ef6f0441ec21de23eb917422cd2e123199002
arpadzsoltjakab <arpadzsoltjakab@gmail.com>
2015-10-11 20:51:20 +0200

    add loops to project

---
+++ hello-world/hello-world/loops/SimpleLoop.java
@@ -0,0 +1,11 @@
+package loops;
+
+import com.sun.jndi.url.dns.dnsURLContext;
+
+public class SimpleLoop {
+	public static void main(String[] a){
+		for (int i = 0;i < 10; i++){
+			System.out.println(i);
+		}
+	}
+}
