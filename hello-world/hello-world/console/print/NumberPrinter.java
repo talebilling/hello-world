06a0a89f60ff6a0163d9088bb5561cdda1c2b76f
talebilling <talebilling@gmail.com>
2015-10-20 16:46:14 +0200

    part of the task is ready

--- hello-world/hello-world/console/print/NumberPrinter.java
+++ hello-world/hello-world/console/print/NumberPrinter.java
@@ -5,10 +5,24 @@ public class NumberPrinter {
 		for (int i = 0; i < arg.length; i++) {
 			System.out.println(Integer.parseInt(arg[i]));
 		}
+
+		// ennek a mintájára adja össze a beadott paramétereket, kiirja az összeget!
+
+		//számolja meg hány paramétert kapott, használj ciklust mindig
+
+		//állapítsa meg, hogy a beadott paraméterek között volt-e kettes
+		System.out.println(" ");
+		for (int i = 0; i < arg.length; i++) {
+
+		}
+
+		//írja ki az ellenkezõjét a beadott paramétereknek
+
+		//íraja ki minden ciklusban az indexet, a paraméterek számának megfelelõen
+		System.out.println(" ");
+		for (int i = 0; i < arg.length; i++) {
+			System.out.println("indexszám: " +i);
+			System.out.println(arg[i]);
+		}
 	}
-	// ennek a mintájára adja össze a beadott paramétereket, kiirja az összeget!
-	//számolja meg hány paramétert kapott, használj ciklust mindig
-	//állapítsa meg, hogy a beadott paraméterek között volt-e kettes
-	//írja ki az ellenkezõjét a beadott paramétereknek
-	//íraja ki minden ciklusban az indexet, a paraméterek számának megfelelõen
 }
