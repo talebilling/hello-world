ce0e33b2ba7fdef393c86a2ea298cb9df9c87fde
arpadzsoltjakab <arpadzsoltjakab@gmail.com>
2015-10-20 22:13:22 +0200

    add loops

--- hello-world/hello-world/console/print/NumberPrinter.java
+++ hello-world/hello-world/console/print/NumberPrinter.java
@@ -7,22 +7,37 @@ public class NumberPrinter {
 		}
 
 		// ennek a mintájára adja össze a beadott paramétereket, kiirja az összeget!
-
-		//számolja meg hány paramétert kapott, használj ciklust mindig
-
-		//állapítsa meg, hogy a beadott paraméterek között volt-e kettes
-		System.out.println(" ");
+		System.out.println("The sum of arguments:");
+		int sum = 0;
 		for (int i = 0; i < arg.length; i++) {
-
+			sum = sum + Integer.parseInt(arg[i]);
 		}
+		System.out.println(sum);
+		//számolja meg hány paramétert kapott, használj ciklust mindig
+		System.out.println("The number of arguments:");
+		int counter = 0;
+		for (int i = 0; i < arg.length; i++) {
+			counter++;
+		}
+		System.out.println(counter);
+		
+		//állapítsa meg, hogy a beadott paraméterek között volt-e kettes
+		System.out.println("Is there any two");
+		boolean isThereAnyTwo = false;
+		for (int i = 0; i < arg.length; i++) {
+			if (Integer.parseInt(arg[i]) == 2) {
+				isThereAnyTwo = true;
+			}
+		}
+		System.out.println(isThereAnyTwo);
 
 		//írja ki az ellenkezõjét a beadott paramétereknek
 
 		//íraja ki minden ciklusban az indexet, a paraméterek számának megfelelõen
-		System.out.println(" ");
+		System.out.println("The loop indicies:");
 		for (int i = 0; i < arg.length; i++) {
 			System.out.println("indexszám: " +i);
-			System.out.println(arg[i]);
+			//System.out.println(arg[i]);
 		}
 	}
 }
